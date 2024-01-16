e519fcd147aaab97a14f00b68bfa6ce0ea347924
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-01-16 15:41:43 +0530

    add ModelMapper bean and firebase bucket as bean in AppInitializer

--- src/main/java/lk/ijse/dep11/edupanel/AppInitializer.java
+++ src/main/java/lk/ijse/dep11/edupanel/AppInitializer.java
@@ -1,7 +1,19 @@
 package lk.ijse.dep11.edupanel;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @SpringBootApplication
 public class AppInitializer {
@@ -9,5 +21,24 @@ public class AppInitializer {
     public static void main(String[] args) {
         SpringApplication.run(AppInitializer.class, args);
         System.out.println("Project work");
+    }
+
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    public Bucket defaultBucket(@Value("${application.firebase.storage.bucket}") String storageBucket) throws IOException {
+        InputStream serviceAccount =
+                new ClassPathResource("/firebase.json").getInputStream();
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket(storageBucket)
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) FirebaseApp.initializeApp(options);
+        return StorageClient.getInstance().bucket();
     }
 }
